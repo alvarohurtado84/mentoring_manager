4c78495be3c833138074a9dcdd867b67b79bed92
Alvaro Hurtado <alvarohurtado84@gmail.com>
2013-06-03 23:22:58 +0100

    Updated the Startup model.

--- mentoring_manager/entrepreneurs/models.py
+++ mentoring_manager/entrepreneurs/models.py
@@ -17,19 +17,61 @@
 
 from django.db import models
 
+from django.utils.translation import ugettext as _
+
 from people.models import Person
 
+STARTUP_STATE_CHOICES = [
+    ["i", _("Idea")],
+    ["bmd", _("Business model development")],
+    ["mvpd", _("MVP Development")],
+    ["bmv", _("Business model validation")],
+    ["g", _("Growth")],
+]
 
 class Startup(models.Model):
     """Contains the startups data."""
     
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    
+    state = models.CharField(
+        max_length=100,
+        choices=STARTUP_STATE_CHOICES)
+    
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    avatar = models.ImageField(upload_to="avatars/startups",
+                               blank=True, null=True)
+
+    # What is the product or service you are building?
+    about_product = models.TextField(blank=True, null=True)
+    # Who is the customer, what problem are you solving?
+    about_customer = models.TextField(blank=True, null=True)
+    # Competitor and why people are going to prefer your product
+    about_competitors = models.TextField(blank=True, null=True)
     
-    avatar = models.ImageField(upload_to="avatars/startups")
-    web = models.URLField()
+    # Website, video, twitter, presentation, anything else
+    webs = models.TextField(blank=True, null=True)
     
-    twitter_user = models.CharField(max_length=50)
+    # Anything else you want to comment?
+    more = models.TextField(blank=True, null=True)
+  
+    
+    #
+    # Private information for mentors
+    #
+    
+    # When did you starto working on it?
+    when_start = models.CharField(max_length=100, blank=True, null=True)
+    legally_constituted = models.BooleanField()
+    # How will you make money?
+    business_model = models.TextField(blank=True, null=True)
+    # Which is your commitment with the project?
+    commitment = models.TextField(blank=True, null=True)
+    # Resource (how many people in team, funds, already spent...)
+    resources = models.TextField(blank=True, null=True)
+    # Short, mid and long-term goals
+    goals = models.TextField(blank=True, null=True)
 
 
 class Entrepreneur(models.Model):
